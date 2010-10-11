7b1a025e5fa12da694877b0746b180a30d117b12
Ian Ward <ianshward@gmail.com>
2010-10-11 16:50:02 -0400

    merge in d7 changes

--- ds_feedback.js
+++ ds_feedback.js
@@ -1,21 +1,27 @@
 /**
  * Behavior for expanding/collapsing feedback block.
  */
-Drupal.behaviors.DSFeedback = function(context) {
 
-  $('#block-boxes-ds_feedback:not(.ds-feedback-processed)').each(function() {
-    $(this).addClass('ds-feedback-processed');
-    $('.block-title', this).click(function() {
-      var block = $(this).parents('div.block');
-      if (!$(block).is('.ds-feedback-active')) {
-        $('.block-content', block).slideDown('fast');
-        $(block).addClass('ds-feedback-active');
-      }
-      else {
-        $(block).removeClass('ds-feedback-active');
-        $('.block-content', block).slideUp('fast');
-      }
-    });
-  });
+(function ($) {
+	
+	Drupal.behaviors.DSFeeback = {};
+	Drupal.behaviors.DSFeeback.attach = function(context) {
 
-}
+		$('#block-boxes-ds-feedback:not(.ds-feedback-processed)').each(function() {
+			$(this).addClass('ds-feedback-processed');
+			$('.block-title', this).click(function() {
+				var block = $(this).parents('div.block');
+				if (!$(block).is('.ds-feedback-active')) {
+					$('.block-content', block).slideDown('fast');
+					$(block).addClass('ds-feedback-active');
+				}
+				else {
+					$(block).removeClass('ds-feedback-active');
+					$('.block-content', block).slideUp('fast');
+				}
+			});
+		});
+
+	}
+
+})(jQuery);
